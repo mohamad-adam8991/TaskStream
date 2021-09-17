2a5e3be1aeec6f26b5a065fd7de3e1f08563c59b
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-09-17 17:38:40 +0800

    Add operations using lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -6,6 +6,7 @@ import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -15,14 +16,16 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        //System.out.println("Printing deadlines");
-        //printDeadlines(tasksData);
+        System.out.println("Printing deadlines");
+        printDeadlines(tasksData);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-        //printData(tasksData);
-        //printDataWithStreams(tasksData);
+        printData(tasksData);
+        printDataWithStreams(tasksData);
         printDeadlinesUsingStreams(tasksData);
-        System.out.println("Total number of deadlines (using streams): " + countDeadlinesUsingStreams(tasksData));
+        ArrayList<Task> filteredList = filterTasksByString(tasksData, "11");
+        printData(filteredList);
     }
+
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
         int count = 0;
@@ -64,10 +67,16 @@ public class Main {
     }
 
     public static void printDeadlinesUsingStreams(ArrayList<Task> tasks) {
-        System.out.println("Printing deadlines using stream");
         tasks.stream()
-                .filter((t) -> t instanceof Deadline)//filtering using lambda
+                .filter((t) -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
                 .forEach(System.out::println);
     }
 
+    public static ArrayList<Task> filterTasksByString(ArrayList<Task> tasksData, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasksData.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(Collectors.toList());
+        return filteredList;
+    }
 }
